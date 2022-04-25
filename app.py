cde64a1edefe58dfadbb636312aa55b60045fc58
fosterea <foster.angus@gmail.com>
2022-04-24 22:11:15 -0700

    first commit

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
 import os
-
+import time
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Prompt
+with open('input.txt', 'r') as f:
+    input = f.read()
+max_context_len = 100
+users = {}
+max_propmpts_per_min = 15
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        context = request.form["context"]
+        id = request.form["id"]
+
+        # Limits requests per minute
+        # Add new users or reset old users after a minute
+        if id not in users or users[id]['time'] + 60 < time.time():
+            users[id] = {'num': 0, 'time': time.time()}
+        # Keep track of querys
+        users[id]['num'] += 1
+        # Block user if there are too many querys
+        if users[id]['num'] > max_propmpts_per_min:
+            return redirect(url_for("index", error="Too many prompts. Wait a minute."))
+        
+
+        error = check_input(context)
+        if error != None:
+            return redirect(url_for("index", error=error))
+
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(context),
+            temperature=.2,
+            max_tokens=50,
+            stop=["context:"],
+            user=id
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+    error = request.args.get("error")
+    return render_template("index.html", result=result, error=error)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(context):
+    return input + context.strip() + "\njoke: "
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def check_input(context):
+    """Checks if the user input aligns with
+    openai's policies."""
+    if len(context.strip()) > max_context_len:
+        return f"Context must be fewer than {max_context_len} characters."
+    if len(context.strip()) == 0:
+        return "Please enter more input."
+    return None
+
+app.run()
---
+++ static/user.js
@@ -0,0 +1,34 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var id = getCookie('id');
+    if (id == '') {
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + today.getMilliseconds();
+        var dateTime = date+' '+time;
+        id = dateTime + Math.random()
+    }
+    setCookie("id", id, 365) // Update cookie to keep it stored
+    document.querySelector("#id").value = id;
+});
