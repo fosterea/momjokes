2559ee4477880c755e6af46271b2c2836fdf7f10
fosterea <foster.angus@gmail.com>
2022-04-26 21:16:53 -0700

    Censor sh*t

--- app.py
+++ app.py
@@ -78,7 +78,14 @@ def query():
         stop=["context:"],
         user=id
     )
-    return {"result":response.choices[0].text}
+
+    output = response.choices[0].text
+    # Check with content filter
+    error = check_output(output)
+    if error != None:
+        return {"error": error}
+
+    return {"result":output}
 
 
 
@@ -92,6 +99,58 @@ def check_input(context):
         return f"Context must be fewer than {max_context_len} characters."
     if len(context.strip()) == 0:
         return "Please enter more input."
+    return None
+
+def check_output(output):
+    """Returns any error associated with a bad output."""
+    response = openai.Completion.create(
+        engine="content-filter-alpha",
+        prompt = "<|endoftext|>"+output+"\n--\nLabel:",
+        temperature=0,
+        max_tokens=1,
+        top_p=0,
+        logprobs=10
+    )
+    output_label = response["choices"][0]["text"]
+
+    # This is the probability at which we evaluate that a "2" is likely real
+    # vs. should be discarded as a false positive
+    toxic_threshold = -0.355
+
+    if output_label == "2":
+        # If the model returns "2", return its confidence in 2 or other output-labels
+        logprobs = response["choices"][0]["logprobs"]["top_logprobs"][0]
+
+        # If the model is not sufficiently confident in "2",
+        # choose the most probable of "0" or "1"
+        # Guaranteed to have a confidence for 2 since this was the selected token.
+        if logprobs["2"] < toxic_threshold:
+            logprob_0 = logprobs.get("0", None)
+            logprob_1 = logprobs.get("1", None)
+
+            # If both "0" and "1" have probabilities, set the output label
+            # to whichever is most probable
+            if logprob_0 is not None and logprob_1 is not None:
+                if logprob_0 >= logprob_1:
+                    output_label = "0"
+                else:
+                    output_label = "1"
+            # If only one of them is found, set output label to that one
+            elif logprob_0 is not None:
+                output_label = "0"
+            elif logprob_1 is not None:
+                output_label = "1"
+
+            # If neither "0" or "1" are available, stick with "2"
+            # by leaving output_label unchanged.
+
+    # if the most probable token is none of "0", "1", or "2"
+    # this should be set as unsafe
+    if output_label not in ["0", "1", "2"]:
+        output_label = "2"
+
+    if output_label == '2':
+        return 'Censored'
     return None
 
 if __name__ == "__main__":
